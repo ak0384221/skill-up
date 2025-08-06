d0000861edf71a5671019a32d9498fb20abe1103
Ayaan <sharingan0384221@gmail.com>
2025-08-06 15:15:59 +0600

    fixed pageloader size

--- src/components/shared/pageLoader.jsx
+++ src/components/shared/pageLoader.jsx
@@ -5,8 +5,8 @@ export default function PageLoader() {
     <>
       <div className="fixed inset-0 z-1000 flex items-center justify-center ">
         <Trefoil
-          size="120"
-          stroke="5"
+          size="80"
+          stroke="2"
           strokeLength="0.15"
           bgOpacity="0.1"
           speed="1"
